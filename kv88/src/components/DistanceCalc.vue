9588ce476fe8112e4f42a308ab4a6bb39574f04d
L_General_RohrReich <71799901+AlexanderKokhno@users.noreply.github.com>
2021-10-26 22:04:43 -0400

    rewrwr

--- kv88/src/components/DistanceCalc.vue
+++ kv88/src/components/DistanceCalc.vue
@@ -1,20 +1,26 @@
 <template>
     <div class="DistanceCalcDiv">
-        <div class="ShellType">
-            <p>Select Measurement Unit(Of Coffee Drank)
+        <div class="roundType">
+            <p>Select Round (mm)(name)(type)
             </p>
-            <form>
+            <!-- <form>
                 <select name="ShellDPn" id="ShellDPn" v-on:change="Calc_Distance" >
                     <option id="PLCH" selected>Select Shell Type Here</option>
-                    <option value="KLm" id="KV_AC" v-on:click="Test1">{{STA1[0]}}</option>
+                    <option value="KLm" id="KV_AC" v-on:click="testfunc1">{{STA1[0]}}</option>
                     <option value="OBm" id="OB_MU">{{STA1[1]}}</option>
                     <option value="MLm" id="ML_MU">{{STA1[2]}}</option>
                     <option value="MLm2" id="ML_MU2">{{STA1[3]}}</option>
                     <option value="MLm3" id="ML_MU3">{{STA1[4]}}</option>
                     <option value="MLm4" id="ML_MU4">{{STA1[5]}}</option>
                 </select>
-            </form>
-            <button v-on:click="Calc_Distance" id="Calc_Btn">Calculate</button>
+            </form> -->
+            <div>
+              <button @click="dropDown">Calculate</button>
+              <select v-model="selected" class="form-control sl">
+              <option v-for="option in options" :value="option.value" :key="option.value">{{ option.name }}</option>
+              </select>
+            </div>
+            <!-- <button v-on:click="Calc_Distance" id="Calc_Btn">Calculate2</button> -->
         </div>
     </div>
 </template>
@@ -27,17 +33,55 @@ export default {
   },
   data () {
     return {
-      distance: 7,
-      STA1: ['152mm G-530 - The Anti-Concrete', '88mm (PzGr 39/43) - APCBC', '128mm 12,8/8,8 Pzgr.TS - APCBC', '120mm T14E3 - APCBC', '120mm DM43 - APFSDS', '120mm 3BK18M - HEATFS']
+      testvar1: 5,
+      testvar2: 5.5,
+      testvar3: 6.43442,
+      distance: 0,
+      // STA1:
+      // ['152mm G-530 - The Anti-Concrete', '88mm (PzGr 39/43) - APCBC', '128mm 12,8/8,8 Pzgr.TS - APCBC', '120mm T14E3 - APCBC', '120mm DM43 - APFSDS', '120mm 3BK18M - HEATFS'
+      // ],
       // gei_kv2_ac: document.getElementById('KV_AC')
+      options: [],
+      selected: ''
     }
   },
   methods: {
+    dropDown: function () {
+      var DDFV = this
+      var types = [
+        {
+          name: '152mm G-530 - The Anti-Concrete',
+          value: '400'
+        },
+        {
+          name: '88mm (PzGr 39/43) - APCBC',
+          value: '300'
+        },
+        {
+          name: '128mm 12,8/8,8 Pzgr.TS - APCBC',
+          value: '200'
+        },
+        {
+          name: '120mm T14E3 - APCBC',
+          value: '290'
+        },
+        {
+          name: '120mm DM43 - APFSDS',
+          value: '590'
+        },
+        {
+          name: '120mm 3BK18M - HEATFS',
+          value: '490'
+        }
+      ]
+      DDFV.options = types
+      console.log(DDFV.selected)
+    },
     Calc_Distance: function () {
-      this.distance *= 1.34
+      this.testvar1 *= 1.34
       console.log(this.distance)
     },
-    Test1: function () {
+    testfunc1: function () {
       console.log(this.STA1[1])
     }
   }
